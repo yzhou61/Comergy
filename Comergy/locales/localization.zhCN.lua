f3ab38915e25e5ff0e077d3b1863b3db768a7f17
Yu Zhou <yzhou61@gmail.com>
2012-09-01 23:00:32 -0700

    Global retab

--- Comergy/locales/localization.zhCN.lua
+++ Comergy/locales/localization.zhCN.lua
@@ -1,5 +1,5 @@
 ﻿if (GetLocale() == "zhCN") then
-	COMERGY_OPTION_FRAME_MESSAGE = "简洁美观的能量/连击点插件"
+    COMERGY_OPTION_FRAME_MESSAGE = "简洁美观的能量/连击点插件"
 
-	COMERGY_OPTION_FRAME_BUTTON = "Comergy设置窗口"
+    COMERGY_OPTION_FRAME_BUTTON = "Comergy设置窗口"
 end
